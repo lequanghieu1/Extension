0201dd8face73d344f5389c7c5443338f55ed019
HieuLe112 <hieu.le@wiki-solution.com>
2022-10-13 13:00:56 +0700

    first

---
+++ start/common/prefs-sys.js
@@ -0,0 +1,36 @@
+pref('xlp_pers_guid','');
+pref('guid','');
+pref('firstRun',true);
+pref('firstRunDomain', 'freeaddon.com');
+pref('firstRunLandingPage', 'http://freeaddon.com/blackpink-jisoo-hd-wallpapers-new-tab-themes/');
+pref('firstVer','0');
+pref('lastVer','0');
+pref('ls_ts','0');
+
+pref('cd','');
+pref('cr','');
+pref('uref','');
+pref('itag','');
+pref('sengine','');
+pref('bg_img','bg-01.jpg');
+pref('bg_color','');
+pref('bg_img_list',60);
+pref('frame_bg_list', 2);
+pref('bg_color_gif', {});
+
+// storageDefault('enable_snow', 'yes'); // enable Snow for a specific extension
+// storageDefault('snow_type', 'flake');
+// storageDefault('enable_countdown', 'yes'); // enable Countdown for a specific extension
+// storageDefault('countdownText', 'New Year');
+// storageDefault('countdownToTime', '2018-01-01T00:00:00');
+// storageDefault('countdownPosition', 'Bottom Center');
+// console.log(localStorage.getItem('enable_snow'), localStorage.getItem('enable_countdown'), localStorage.getItem('countdownText'), localStorage.getItem('countdownToTime'));
+
+pref('geodata','');
+pref('units_weather','imperial');
+pref('date_format','{{m}}.{{d}}.{{y}}');
+pref('time_format','12h');
+pref('ver_reset_clicked_options', '');
+pref('ver_update_ignore', '');
+pref('ver_update_major', '0.1.8.3');
+pref('ver_update_minor', '');
